ddcbfc2dae3f65f9193cd24a1f89e13fbda2f2cd
qianzhaoyan <779547508@qq.com>
2017-06-07 13:49:44 +0800

    联系我

--- src/view/connection.vue
+++ src/view/connection.vue
@@ -3,51 +3,105 @@
    		<el-col :xs="{span:22,offset:1}" :sm="{span:16, offset:4}" class="info">
    		  <div class="title">
    		    <h1>Contact Infomation</h1>
+   		    <div class="maskleft"></div>
+   		    <div class="maskright"></div>
+   		  </div>
+   		  
+   		  <div class="connect">
+					<h2>QQ</h2>
+					<div>779547508</div>
+   		  </div>
+   		  
+   		  <div class="connect">
+					<h2>邮箱</h2>
+					<div>779547508@qq.com</div>
+   		  </div>
+   		  
+   		  <div class="connect">
+					<h2>Github</h2>
+					<div><a href="https://github.com/qianzhaoy">https://github.com/qianzhaoy</a></div>
    		  </div>
    		</el-col>
    </el-row>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {}
-    },
-    components: {}
-  }
+	export default {
+		data() {
+			return {}
+		},
+		components: {}
+	}
 
 </script>
 
 <style lang="less" scoped>
-  #connection {
-    font-size: .18rem;
-    margin-top: .5rem;
-  }
-  
-  .info {
-    h1 {
-      position: absolute;
-      top: .05rem;
-      left: -5%;
-      font-size: .24rem;
-      line-height: 1rem;
-      font-weight: 700;
-      text-indent: 2em;
-      width: 110%;
-      height: 1rem;
-      background-color: rgb(147, 224, 255);
-      transition: all 2s;
-      
-      &:hover{
-        width: 90%;
-      }
-    }
-    div.title {
-      position: relative;
-      height: 1.1rem;
-      border: 1px solid #fff;
-      border-radius: .5rem;
-    }
-  }
+	#connection {
+		font-size: .18rem;
+		margin-top: .5rem;
+	}
+	
+	.info{
+		>div{
+			margin-bottom: .3rem;
+			font-size: .24rem
+		}
+	}
+	
+	div.title {
+		position: relative;
+		height: 1.2rem;
+		border-top: 1px solid #fff;
+		border-bottom: 1px solid #fff;
+		h1 {
+			position: absolute;
+			top: 0;
+			left: 0;
+			text-indent: 2em;
+			line-height: 1.2rem;
+			font-weight: 700;
+			z-index: 9;
+		}
+		.maskleft {
+			position: absolute;
+			top: -1px;
+			left: -1px;
+			height: 1.2rem;
+			background-color: rgb(147, 224, 255);
+			animation: maskShow 2s ease-out
+		}
+		.maskright {
+			position: absolute;
+			top: -1px;
+			right: -1px;
+			height: 1.2rem;
+			background-color: rgb(147, 224, 255);
+			animation: maskShow 2s ease-out
+		}
+	}
+	
+	div.connect{
+		>div{
+			line-height: .8rem;
+			border-radius: .1rem;
+			margin-top: .2rem;
+			background-color: rgba(255, 255, 255, 0.65);
+			text-indent: 2em;
+		}
+		
+		a{
+			text-decoration: underline;
+		}
+	}
+	
+	@keyframes maskShow {
+		0% {
+			width: 50%;
+		}
+		100% {
+			width: 0;
+			display: none;
+		}
+	}
 
 </style>
