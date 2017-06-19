f35000488175dcaa92dfc3d69009a222c49fd63c
qianzhaoyan <779547508@qq.com>
2017-06-19 17:25:41 +0800

    自适应优化

--- src/view/aboutMe.vue
+++ src/view/aboutMe.vue
@@ -3,15 +3,17 @@
   		<Banner></Banner>
    		<el-col :xs="{span:22,offset:1}" :sm="{span:20, offset:2}">
    			<el-row>
-					<el-col :span="18">
+					<el-col :xs="24" :sm="18" >
 						<AboutLeft></AboutLeft>
 					</el-col>
 					
-					 <el-col :span="5" :offset="1">
-						 <AboutRight></AboutRight>
+					<el-col :xs="24" :sm="{span:5, offset:1}">
+						<AboutRight></AboutRight>
 					</el-col>
+					
 				</el-row>
    		</el-col>
+   		
    </el-row>
 </template>
 
@@ -19,22 +21,20 @@
 	import AboutRight from '@/components/aboutRight'
 	import AboutLeft from '@/components/aboutLeft'
 	import Banner from '@/components/banner'
-    export default {
-        data () {
-            return {
-            }
-        },
-        components: {
-					Banner,
-					AboutRight,
-					AboutLeft
-        }
-    }
+	export default {
+		components: {
+			Banner,
+			AboutRight,
+			AboutLeft
+		}
+	}
+
 </script>
 
 <style lang="less" scoped>
-	#aboutMe{
+	#aboutMe {
 		text-align: center;
 		color: #3479ad;
 	}
+
 </style>
